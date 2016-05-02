56bfa1cbec09f1dca4189bcb747ea4122abb561d
xxPicasso <ben@moregolf.co.nz>
2016-05-02 12:48:54 +1200

    bellow borld test

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-import express 'express'
+import express from 'express'
 let app = express()
 let port = process.env.PORT || 8080
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Bellow Borld')
 })
-	
+
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
 })
